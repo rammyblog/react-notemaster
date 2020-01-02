327f8be9e633c4648c6012e267cd3e6cd5fc3bea
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-01-02 04:03:01 +0100

    Fixed the import error in urls.py

--- evernote/urls.py
+++ evernote/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls import url
 from rest_auth import views as auth_views 
-from django.view.generic import TemplaView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -11,5 +11,5 @@ urlpatterns = [
     path('api/', include('notemaster.urls')),
     path('api/auth/', include('rest_auth.urls')),
     path('api/auth/registration/', include('rest_auth.registration.urls')),
-    re_path('.*', TemplaView.as_view(template_name="index.html") )
+    re_path('.*', TemplateView.as_view(template_name="index.html") )
 ]
